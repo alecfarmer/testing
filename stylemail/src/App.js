ddc4a1aa87b0eb06fb64107057bf9a2398420490
Alec Farmer <realalecfarmer@gmail.com>
2020-11-12 02:47:58 -0500

    ich

--- stylemail/src/App.js
+++ stylemail/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          I changed something.,,,
+          I changed
         </p>
         <a
           className="App-link"
