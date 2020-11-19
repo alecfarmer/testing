921278ffbc31c086b903530b656749e4156d9e5c
alecfarmer <realalecfarmer@gmail.com>
2020-11-19 10:42:23 -0500

    check

--- stylemail/src/App.js
+++ stylemail/src/App.js
@@ -16,6 +16,20 @@ firebase.initializeApp({
   appId: "1:899641804209:web:1ee4aaece932abc05fe990"
 })
 
+/*
+firebase.initializeApp({
+  apiKey: "AIzaSyCLs1a1LcEeXEiOAomAG3EodV5Js3LOnAY",
+  authDomain: "stylemail-edf5f.firebaseapp.com",
+  databaseURL: "https://stylemail-edf5f.firebaseio.com",
+  projectId: "stylemail-edf5f",
+  storageBucket: "stylemail-edf5f.appspot.com",
+  messagingSenderId: "506653836700",
+  appId: "1:506653836700:web:b0a2808623db1a3fc0b459",
+  measurementId: "G-F80D8NFME2"
+
+})
+*/
+
 const auth = firebase.auth();
 
 function App() {
