71921870e37838e1a60350ba2a681593a72a96cd
alecfarmer <realalecfarmer@gmail.com>
2020-11-24 18:59:44 -0500

    yy

--- stylemail/src/components/Login.js
+++ stylemail/src/components/Login.js
@@ -2,9 +2,8 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
-import { auth } from "../firebase"
+//import { auth } from "../firebase"
 import app from "../firebase"
-import App from "./App"
 
 export default function Login() {
   const emailRef = useRef()
